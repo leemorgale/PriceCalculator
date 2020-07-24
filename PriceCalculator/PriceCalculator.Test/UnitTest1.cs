fa8eb1e061de61f336d18bfbec8e1bcdb48a1f28
lee.morgan <P95056888@capita.co.uk>
2020-07-24 13:35:23 +0100

    Start of price calculator

---
+++ PriceCalculator/PriceCalculator.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PriceCalculator.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
